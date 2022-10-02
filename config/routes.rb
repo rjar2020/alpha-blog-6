f77aeebe04588814a13406288d0ffcbfd85ff42d
rjar2020 <rolando.angelini@factorial.co>
2022-10-02 19:46:17 +0200

    Restrict mutations for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   resources :articles
   root 'pages#home'
-  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
