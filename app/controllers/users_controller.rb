31d2f9fc5d0c0cd85201b57dce73fa2439bed24d
rjar2020 <rolando.angelini@factorial.co>
2022-10-03 23:22:20 +0200

    When deleting Users, cascade to Articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,9 +54,9 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-
+    session[:user_id] = nil
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to articles_path, notice: "User and their associated articles were successfully deleted." }
       format.json { head :no_content }
     end
   end
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @auser
+      if current_user != @user
         flash[:alert] = "You can only edit your own profile"
         redirect_to @user
       end
