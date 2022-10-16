029719c3183dfa84b1308b0ba42eb93cb9a6c711
rjar2020 <rolando.angelini@factorial.co>
2022-10-16 12:55:29 +0200

    Implementing Admin users requirements for managing Categories, using TDD

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin = User.create(username: "johndoe", email: "johndoe@example.com",
+                        password: "password", admin: true)
+    sign_in_as(@admin)
+  end
   
   test "get new category form and create category" do
     get "/categories/new"
